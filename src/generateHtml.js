ad051d79a65a0e40afbae8faad1aa6aa5994fd45
Dima <strayo4ko@mail.ru>
2017-11-11 22:57:33 +0300

    Ver 1

---
+++ src/generateHtml.js
@@ -0,0 +1,14 @@
+var maxSections = 5;
+var maxArticles = 3;
+for(var section=0; section<maxSections; section++){
+    document.write("<section>");
+    for(var article = 0; article<maxArticles; article++){
+        document.write("<article>">;
+        document.write("<img src=\"img/"+section+"i"+article+".jpg\" class=\"titleImage\">");               
+                    document.write("<img class=\"buttonPlus\">");
+                   document.write("<img class=\"buttonMinus\">")''
+        document.write("<article>">;
+        );
+    }
+    
+}
